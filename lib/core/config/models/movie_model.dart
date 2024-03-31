5433320cc12427f1f05d60fced8e167d292a9ef7
mostafa <mostafarasekh5@gamil.com>
2024-03-31 04:29:09 +0200

    create function of firebase

--- lib/core/config/models/movie_model.dart
+++ lib/core/config/models/movie_model.dart
@@ -7,6 +7,7 @@ class MovieModel {
   final String releaseDate;
   final double voteAverage;
   List<String>? genresList;
+  String? cloudId;
 
   MovieModel({
     this.genresList,
@@ -17,6 +18,7 @@ class MovieModel {
     required this.posterImage,
     required this.releaseDate,
     required this.voteAverage,
+    this.cloudId,
   });
   factory MovieModel.fromJson(Map<String, dynamic> json) {
     return MovieModel(
@@ -27,6 +29,20 @@ class MovieModel {
       posterImage: 'https://image.tmdb.org/t/p/w500${json['poster_path']}',
       releaseDate: json['release_date'],
       voteAverage: json['vote_average'],
+      cloudId: json['cloudId'],
     );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'title': title,
+      'id': id,
+      'overview': overview,
+      'backdropImage': backdropImage,
+      'posterImage': posterImage,
+      'releaseDate': releaseDate,
+      'voteAverage': voteAverage,
+      'cloudId': cloudId,
+    };
   }
 }
--- lib/core/widgets/unfavorite_bookmark.dart
+++ lib/core/widgets/unfavorite_bookmark.dart
@@ -1,24 +1,43 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/config/models/movie_model.dart';
+import 'package:movies_app/core/config/services/cloud_firestore.dart';
 
 class UnSelectedBookmark extends StatelessWidget {
   const UnSelectedBookmark({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Stack(
-      alignment: Alignment.center,
-      children: [
-        Icon(
-          Icons.bookmark,
-          size: 30,
-          color: Color(0xff514f4f),
-        ),
-        Icon(
-          Icons.add,
-          color: Colors.white,
-          size: 20,
-        )
-      ],
+    return IconButton(
+      onPressed: () async {
+        MovieModel movie = MovieModel(
+          id: 1,
+          backdropImage: 'test',
+          title: 'title',
+          overview: 'overview',
+          posterImage: 'posterImage',
+          releaseDate: 'releaseDate',
+          voteAverage: 7.2,
+        );
+        await FireStoreService().addMovie(movie);
+        log('added');
+      },
+      icon: const Stack(
+        alignment: Alignment.center,
+        children: [
+          Icon(
+            Icons.bookmark,
+            size: 30,
+            color: Color(0xff514f4f),
+          ),
+          Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 20,
+          )
+        ],
+      ),
     );
   }
 }
